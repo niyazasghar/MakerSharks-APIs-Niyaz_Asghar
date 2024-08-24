d9987879aa04382496cbcda4bf208d8d91f8c35d
Md Niyaz Asghar <niyazasghar12@gmail.com>
2024-08-24 22:57:22 +0530

    Delete Version-1/API-Makersharks-version-1 directory

--- Version-1/API-Makersharks-version-1/src/main/java/Assignment/API/entity/ManufacturingProcess.java
+++
@@ -1,5 +0,0 @@
-package Assignment.API.entity;
-
-public enum ManufacturingProcess {
-    MOULDING, THREE_D_PRINTING, CASTING, COATING
-}
--- Version-1/API-Makersharks-version-1/src/main/java/Assignment/API/entity/NatureOfBusiness.java
+++
@@ -1,5 +0,0 @@
-package Assignment.API.entity;
-
-public enum NatureOfBusiness {
-    SMALL_SCALE, MEDIUM_SCALE, LARGE_SCALE
-}
